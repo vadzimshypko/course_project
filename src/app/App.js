238853526084181bed7dbf58d23dcfa37e037420
@Shypko <vadimshipko@gmail.com>
2018-12-14 15:56:05 +0300

    add restart field

--- src/app/App.js
+++ src/app/App.js
@@ -1,94 +1,89 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "./App.css";
 import Field from "./field/Field";
-import { Button, Input } from "semantic-ui-react";
+import {Button, Input} from "semantic-ui-react";
+import styled from 'styled-components'
+const Header = styled.header`
+  background-color: #282c34;
+  display: flex;
+  min-height: 80px;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  justify-content: center;
+  font-size: calc(50px + 2vmin);
+  color: green;
+  `
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { sizeField: 50, nowTime: 0 };
+    constructor(props) {
+        super(props);
+        this.state = {
+            sizeField: 100,
+            createFieldTouched: false,
+        };
 
-    this.onIncrease = this.onIncrease.bind(this);
-    this.onDecrease = this.onDecrease.bind(this);
-    this.onChangeSizeField = this.onChangeSizeField.bind(this);
-    this.createMineField = this.createMineField.bind(this);
-    this.onUpdateTime = this.onUpdateTime.bind(this);
-
-    setInterval(this.onUpdateTime, 1000);
-  }
-
-  onIncrease() {
-    this.setState({
-      sizeField: parseInt(this.state.sizeField, 0) + 1,
-      nowTime: this.state.nowTime
-    });
-  }
-
-  onDecrease() {
-    this.setState({
-      sizeField: parseInt(this.state.sizeField, 0) - 1,
-      nowTime: this.state.nowTime
-    });
-  }
-
-  onChangeSizeField(event) {
-    this.setState({
-      sizeField: event.target.value !== "" ? event.target.value : 0,
-      nowTime: this.state.nowTime
-    });
-  }
-
-  createMineField() {
-    if (this.state.sizeField < 2) {
-      console.log("Поле слишком маленькое, введите значение больше 1");
-    } else if (this.state.sizeField > 10) {
-      console.log("Поле слишком большое, введите значение меньше 11");
-    } else {
-      console.log("Coздать поле");
-      this.refs.canvasField.setNewField(this.state.sizeField);
+        this.onIncrease = this.onIncrease.bind(this);
+        this.onDecrease = this.onDecrease.bind(this);
+        this.onChangeSizeField = this.onChangeSizeField.bind(this);
+        this.createMineField = this.createMineField.bind(this);
     }
-  }
 
-  onUpdateTime() {
-    this.setState({
-      sizeField: this.state.sizeField,
-      nowTime: this.state.nowTime + 1
-    });
-    this.refs.canvasField.iteration();
-  }
+    onIncrease() {
+        this.setState({sizeField: parseInt(this.state.sizeField, 0) + 1});
+    }
 
-  render() {
-    return (
-      <div className="app">
-        <header className="app-header">Жизнь</header>
-        <h1>Прошло {this.state.nowTime} секунд с начала</h1>
-        <h2>Размер поля: {this.state.sizeField}</h2>
-        <div>
-          <Button color="green" className="button" onClick={this.onDecrease}>
-            -
-          </Button>
-          <Input
-            className="input-field"
-            type="number"
-            value={this.state.sizeField}
-            onChange={this.onChangeSizeField}
-          />
-          <Button color="green" className="button" onClick={this.onIncrease}>
-            +
-          </Button>
-        </div>
-        <Button color="green" className="button" onClick={this.createMineField}>
-          Создать минное поле
-        </Button>
-        <Field
-          id="canvasField"
-          width={400}
-          hight={400}
-          sizeField={this.state.sizeField}
-        />
-      </div>
-    );
-  }
+    onDecrease() {
+        this.setState({sizeField: parseInt(this.state.sizeField, 0) - 1});
+    }
+
+    onChangeSizeField(event) {
+        this.setState({sizeField: event.target.value !== "" ? event.target.value : 0});
+    }
+
+    createMineField() {
+        if (this.state.sizeField < 2) {
+            console.log("Поле слишком маленькое, введите значение больше 1");
+        } else {
+            this.setState({createFieldTouched: true})
+            console.log("Создать поле");
+        }
+    }
+
+    onClearField = () => {
+        this.setState({createFieldTouched: false})
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <Header>Жизнь</Header>
+                <h2>Размер поля: {this.state.sizeField}</h2>
+                <div>
+                    <Button color="green" className="button" onClick={this.onDecrease}>
+                        -
+                    </Button>
+                    <Input
+                        className="input-field"
+                        type="number"
+                        value={this.state.sizeField}
+                        onChange={this.onChangeSizeField}
+                    />
+                    <Button color="green" className="button" onClick={this.onIncrease}>
+                        +
+                    </Button>
+                </div>
+                <Button color="green" className="button" onClick={this.createMineField}>
+                    Создать минное поле
+                </Button>
+                <Field
+                    sizeField={this.state.sizeField}
+                    createFieldTouched={this.state.createFieldTouched}
+                    onClearField={this.onClearField.bind(this)}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/app/field/Field.js
+++ src/app/field/Field.js
@@ -1,115 +1,139 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import './Field.css';
 
 class Field extends Component {
-  constructor(props) {
-    super(props);
-    let tempField = [];
-    for (var i = 0; i < props.sizeField; i++) {
-      tempField[i] = [];
-      for (var j = 0; j < props.sizeField; j++) {
-        tempField[i][j] = (j + i) % 2 === 1 ? true : false;
-      }
-    }
-    this.state = { sizeField: props.sizeField, field: tempField };
-    this.iteration = this.iteration.bind(this);
-    this.setNewField = this.setNewField.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    setInterval(this.iteration, 1000);
-  }
-
-  setNewField(sizeNewField) {
-    let tempField = [];
-    for (var i = 0; i < sizeNewField; i++) {
-      tempField[i] = [];
-      for (var j = 0; j < sizeNewField; j++) {
-        tempField[i][j] = (j + i) % 5 < 2 ? true : false;
-      }
-    }
-    this.setState({ sizeField: sizeNewField, field: tempField });
-  }
-
-  iteration() {
-    let newField = [];
-    for (let i = 0; i < this.state.sizeField; i++) {
-      newField[i] = [];
-      for (let j = 0; j < this.state.sizeField; j++) {
-        newField[i][j] = false;
-      }
-    }
-    for (let i = 0; i < this.state.sizeField; i++) {
-      for (let j = 0; j < this.state.sizeField; j++) {
-        let c = 0;
-        if (i - 1 >= 0 && j - 1 >= 0 && this.state.field[i - 1][j - 1] === true)
-          c += 1;
-        if (j - 1 >= 0 && this.state.field[i][j - 1] === true) c += 1;
-        if (
-          i + 1 < this.state.sizeField &&
-          j - 1 >= 0 &&
-          this.state.field[i + 1][j - 1] === true
-        )
-          c += 1;
-        if (i + 1 < this.state.sizeField && this.state.field[i + 1][j] === true)
-          c += 1;
-        if (i - 1 >= 0 && this.state.field[i - 1][j] === true) c += 1;
-        if (
-          j + 1 < this.state.sizeField &&
-          i - 1 >= 0 &&
-          this.state.field[i - 1][j + 1] === true
-        )
-          c += 1;
-        if (j + 1 < this.state.sizeField && this.state.field[i][j + 1] === true)
-          c += 1;
-        if (
-          i + 1 < this.state.sizeField &&
-          j + 1 < this.state.sizeField &&
-          this.state.field[i + 1][j + 1] === true
-        )
-          c += 1;
-
-        if (this.state.field[i][j] === false && c === 3) {
-          newField[i][j] = true;
+    constructor(props) {
+        super(props);
+        let tempField = [];
+        for (let i = 0; i < props.sizeField; i++) {
+            tempField[i] = [];
+            for (let j = 0; j < props.sizeField; j++) {
+                tempField[i][j] = (j + i) % 2 === 1;
+            }
         }
-        if (this.state.field[i][j] === true && (c === 2 || c === 3)) {
-          newField[i][j] = true;
-        }
-      }
+        this.state = {
+            sizeField: props.sizeField,
+            field: tempField,
+            nowTime: 0,
+            createFieldTouched: false,
+        };
+        this.iteration = this.iteration.bind(this);
+        this.setNewField = this.setNewField.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        setInterval(this.iteration, 1000);
     }
-    this.setState({ sizeField: this.state.sizeField, field: newField });
-    this.updateCanvas();
-  }
 
-  componentDidMount() {
-    this.updateCanvas();
-  }
+    static getDerivedStateFromProps(props) {
+        console.log(props.createFieldTouched)
+        return {
+            createFieldTouched: props.createFieldTouched,
+        }
+    }
 
-  updateCanvas() {
-    const ctx = this.refs.canvas.getContext("2d");
-    let cellLenght = 1000 / this.state.sizeField;
-    for (var i = 0; i < this.state.sizeField; i++) {
-      for (var j = 0; j < this.state.sizeField; j++) {
-        ctx.fillStyle = this.state.field[i][j] ? "green" : "black";
-        ctx.fillRect(
-          cellLenght * i,
-          cellLenght * j,
-          cellLenght - 1,
-          cellLenght - 1
+    setNewField(sizeNewField) {
+        console.log('Create new field');
+        let tempField = [];
+        for (let i = 0; i < sizeNewField; i++) {
+            tempField[i] = [];
+            for (let j = 0; j < sizeNewField; j++) {
+                tempField[i][j] = (j + i) % 2 === 1;
+            }
+        }
+        this.setState({sizeField: sizeNewField, field: tempField, nowTime: 0, createFieldTouched: false });
+    }
+
+    iteration() {
+        let newField = [];
+        for (let i = 0; i < this.state.sizeField; i++) {
+            newField[i] = [];
+            for (let j = 0; j < this.state.sizeField; j++) {
+                newField[i][j] = false;
+            }
+        }
+        for (let i = 0; i < this.state.sizeField; i++) {
+            for (let j = 0; j < this.state.sizeField; j++) {
+                let c = 0;
+                if (i - 1 >= 0 && j - 1 >= 0 && this.state.field[i - 1][j - 1] === true)
+                    c += 1;
+                if (j - 1 >= 0 && this.state.field[i][j - 1] === true) c += 1;
+                if (
+                    i + 1 < this.state.sizeField &&
+                    j - 1 >= 0 &&
+                    this.state.field[i + 1][j - 1] === true
+                )
+                    c += 1;
+                if (i + 1 < this.state.sizeField && this.state.field[i + 1][j] === true)
+                    c += 1;
+                if (i - 1 >= 0 && this.state.field[i - 1][j] === true) c += 1;
+                if (
+                    j + 1 < this.state.sizeField &&
+                    i - 1 >= 0 &&
+                    this.state.field[i - 1][j + 1] === true
+                )
+                    c += 1;
+                if (j + 1 < this.state.sizeField && this.state.field[i][j + 1] === true)
+                    c += 1;
+                if (
+                    i + 1 < this.state.sizeField &&
+                    j + 1 < this.state.sizeField &&
+                    this.state.field[i + 1][j + 1] === true
+                )
+                    c += 1;
+
+                if (this.state.field[i][j] === false && c === 3) {
+                    newField[i][j] = true;
+                }
+                if (this.state.field[i][j] === true && (c === 2 || c === 3)) {
+                    newField[i][j] = true;
+                }
+            }
+        }
+        this.setState({sizeField: this.state.sizeField, field: newField, nowTime: this.state.nowTime + 1});
+        this.updateCanvas();
+    }
+
+    componentDidMount() {
+        this.updateCanvas();
+    }
+
+    updateCanvas() {
+        if(this.state.createFieldTouched) {
+            this.setNewField(this.props.sizenpField);
+            this.props.onClearField();
+        }
+
+        const ctx = this.refs.canvas.getContext("2d");
+        let cellLength = Math.floor(512 / this.state.sizeField);
+        for (let i = 0; i < this.state.sizeField; i++) {
+            for (let j = 0; j < this.state.sizeField; j++) {
+                ctx.fillStyle = this.state.field[i][j] ? "green" : "black";
+                ctx.fillRect(
+                    cellLength * i,
+                    cellLength * j,
+                    cellLength ,
+                    cellLength
+                );
+
+            }
+        }
+    }
+
+    handleClick() {
+        console.log("click");
+    }
+
+    render() {
+        return (<div>
+                <h1>Прошло {this.state.nowTime} секунд с начала</h1>
+                <canvas
+                    ref="canvas"
+                    width={512}
+                    height={512}
+                    onClick={this.handleClick}
+                />
+            </div>
         );
-      }
     }
-  }
-
-  handleClick() {
-    console.log("click");
-  }
-  render() {
-    return (
-      <canvas
-        ref="canvas"
-        className="canvas-field"
-        onClick={this.handleClick}
-      />
-    );
-  }
 }
 
 export default Field;
